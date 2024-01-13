b6b2e3e2ea7aeb6526e57a303f880d08c8bed9bf
Henrique Griepp <henriquegriepp@DESKTOP-SU6BTHJ>
2024-01-13 17:04:49 -0300

    Populating profile data dynamically

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,2 +1,7 @@
 
 
+async function fetchProfileData (){
+  const url = 'https://raw.githubusercontent.com/henrique-griepp/portifolio/main/data/profile.json'
+  const fetchin = await fetch(url)
+  return await fetchin.json()
+}
